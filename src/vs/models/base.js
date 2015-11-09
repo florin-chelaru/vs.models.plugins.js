6fd3c2fd3cac5ca6f062068127b74e73cc832cf4
Florin Chelaru <florin.chelaru@yahoo.com>
2015-11-09 15:37:00 -0500

    provide vs.models only in compiled mode

--- src/vs/models/base.js
+++ src/vs/models/base.js
@@ -9,7 +9,9 @@
  * @suppress {globalThis}
  */
 
-goog.provide('vs.models');
+if (COMPILED) {
+  goog.provide('vs.models');
+}
 
 // Extend the namespaces defined here with the ones in the base vis.js package
 (function() {
