552a3378b7b0df3b1351a6ba34272e325bec4671
Florin Chelaru <florin.chelaru@yahoo.com>
2015-11-09 17:54:53 -0500

    update to latest version of vis and recompile

---
+++ vs.models.plugins.externs.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Florin Chelaru ( florin [dot] chelaru [at] gmail [dot] com )
+ * Date: 10/29/2015
+ * Time: 3:51 PM
+ */
+
+/**
+ * @param {Array.<string>} bigwigURIs
+ * @param {{initialQuery: (vs.models.Query|Array.<vs.models.Query>|undefined), proxyURI: (string|undefined), valsLabel: (string|undefined)}} options
+ * @constructor
+ * @extends {vs.models.DataSource}
+ */
+vs.models.plugins.BigwigDataSource = function(bigwigURIs, options) {};
